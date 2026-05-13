57b57a200d57caaecd9f228ae50f3386558c0fe7
Deepak Singh <dskvg@gmail.com>
2026-05-13 18:50:07 +0530

    day 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 const App = () => {
   return (
     <div>
+      <div>
       hi this is my project for my github for my name 
+      </div>
+      <div>
+        hi this is day 2 
+      </div>
     </div>
+   
   )
 }
 
